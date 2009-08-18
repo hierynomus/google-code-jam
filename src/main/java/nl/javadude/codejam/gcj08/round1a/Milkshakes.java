be1f2ed4acdf3e5a9716dc768507d64af3a8bf3d
Jeroen van Erp <jeroen@Raistlin.lan>
2009-08-18 22:11:51 +0200

    Milshakes geimplementeerd

--- src/main/java/nl/javadude/codejam/gcj08/round1a/Milkshakes.java
+++ src/main/java/nl/javadude/codejam/gcj08/round1a/Milkshakes.java
@@ -1,9 +1,11 @@
 package nl.javadude.codejam.gcj08.round1a;
 
 import java.io.BufferedReader;
+import java.util.Collections;
 import java.util.List;
 
 import nl.javadude.codejam.AbstractMultiProblemSolver;
+import nl.javadude.codejam.gcj08.round1a.MilkshakesProblem.Customer;
 
 public class Milkshakes extends AbstractMultiProblemSolver<MilkshakesProblem> {
 
@@ -13,13 +15,42 @@ public class Milkshakes extends AbstractMultiProblemSolver<MilkshakesProblem> {
 		problem.problemNumber = problemNumber;
 		problem.nrFlavours = readLineAsInt(lines);
 		problem.nrCustomers = readLineAsInt(lines);
-		return null;
+		readLines(lines, problem.nrCustomers, problem.new CustomerClosure());
+		return problem;
 	}
 
 	@Override
 	protected List<String> solveProblem(MilkshakesProblem problem) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean[] shakes = new boolean[problem.nrFlavours + 1];
+		for (int i = 1; i <= problem.nrFlavours; i++) {
+			shakes[i] = false;
+		}
+		
+		boolean needsRechecking = true;
+		boolean impossible = false;
+		while (needsRechecking) {
+			needsRechecking = false;
+			for (Customer c : problem.customers) {
+				if (!c.satisfied(shakes)) {
+					if (c.satisfy(shakes)) {
+						needsRechecking = true;
+					}
+					else {
+						impossible = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if (impossible) {
+			return Collections.singletonList("Case #" + problem.problemNumber + ": IMPOSSIBLE");
+		}
+		StringBuilder builder = new StringBuilder();
+		for (int i = 1; i < shakes.length; i++) {
+			builder.append(" ").append(shakes[i] ? "1" : "0");
+		}
+		return Collections.singletonList("Case #" + problem.problemNumber + ":" + builder.toString());
 	}
 
 }
--- src/test/java/nl/javadude/codejam/CodeJamTest.java
+++ src/test/java/nl/javadude/codejam/CodeJamTest.java
@@ -1,11 +1,11 @@
 package nl.javadude.codejam;
 
-import nl.javadude.codejam.gcj08.round1a.MinimumScalarProduct;
+import nl.javadude.codejam.gcj08.round1a.Milkshakes;
 
 public class CodeJamTest {
 
 	public static void main(String[] args) throws Exception {
-		CodeJam.a = new MinimumScalarProduct();
-		CodeJam.main(new String[] {"src/main/resources/gcj08/round1a/A-large-practice.in"});
+		CodeJam.a = new Milkshakes();
+		CodeJam.main(new String[] {"src/main/resources/gcj08/round1a/B-large-practice.in"});
 	}
 }
