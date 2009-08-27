38080b2ac2569f2414950924b6ae2695633d079f
Jeroen van Erp <ajvanerp@Fizban.local>
2009-08-27 20:51:37 +0200

    Rewritten some for-comprehensions to filters

--- scala/src/main/scala/nl/javadude/scalajam/gcj08/round1a/Milkshakes.scala
+++ scala/src/main/scala/nl/javadude/scalajam/gcj08/round1a/Milkshakes.scala
@@ -4,17 +4,17 @@ import nl.javadude.scalajam.utils.GoogleCodeHelper._
 object Milkshakes extends CodeJam {
 	def solveProblem(reader : Iterator[String]) : String = {
 		val flavours : Array[Boolean] = new Array[Boolean](reader nextInt)
-		val cs = (for (i <- 1 to reader.nextInt; c = reader.nextIntArray.toList.tail) yield
-		(for (j <- 0 until c.size if j % 2 == 0) yield (c(j), c(j+1))).foldLeft(Map[Int, Boolean]()) { (m, t) => m(t._1) = t._2 != 0}).toList
+		val cs = (for (i <- 1 to reader.nextInt; c = reader.nextIntArray) yield
+              (1 until c.size).filter(_ % 2 == 1).map(j => (c(j), c(j+1))).foldLeft(Map[Int, Boolean]()) { (m, t) => m(t._1) = t._2 != 0 }).toList
 
 		var recheck = true
 		var impossible = false
 		while (recheck && !impossible) {
 			recheck = false
-			val ucs = (for (c <- cs if (!isSatisfied(flavours, c))) yield c).toList
+			val ucs = cs filter (!isSatisfied(flavours, _))
 			if (ucs.size != 0) {
 				recheck = true
-				if ((for (c <- ucs if (!satisfy(flavours, c))) yield c).toList.size != 0) {
+				if (ucs.filter(!satisfy(flavours, _)).size != 0) {
 					impossible = true
 				}
 			}
