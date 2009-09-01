8f28ac856bb1f05497b6622b302217c670ade9ca
Jeroen van Erp <ajvanerp@Fizban.local>
2009-09-01 20:53:23 +0200

    Refactored Milkshakes to recursive and functional solution

--- scala/src/main/scala/nl/javadude/scalajam/gcj08/round1a/Milkshakes.scala
+++ scala/src/main/scala/nl/javadude/scalajam/gcj08/round1a/Milkshakes.scala
@@ -11,34 +11,37 @@ object Milkshakes extends CodeJam {
 		val cs = (for (i <- 1 to reader.nextInt; c = reader.nextIntArray) yield
               (1 until c.size).filter(_ % 2 == 1).map(j => (c(j), c(j+1))).foldLeft(Map[Int, Boolean]()) { (m, t) => m(t._1) = t._2 != 0 }).toList
 
-		var recheck = true
-		var impossible = false
-		while (recheck && !impossible) {
-			recheck = false
-			val ucs = cs filter (!isSatisfied(flavours, _))
-			if (ucs.size != 0) {
-				recheck = true
-				if (ucs.filter(!satisfy(flavours, _)).size != 0) {
-					impossible = true
-				}
-			}
+        solve(flavours, cs, unSatisfiedCustomers(flavours, cs)) match {
+        		case None => "IMPOSSIBLE"
+        		case Some(x) => x.map(if (_) "1" else "0").reduceLeft(_ + " " + _)
 		}
-
-		if (impossible) "IMPOSSIBLE"
-		else flavours.map(if (_) "1" else "0").reduceLeft(_ + " " + _)
 	}
  
 	def isSatisfied (flavours : Array[Boolean], customer : Map[Int, Boolean]) : Boolean = {
 		(for ((key, value) <- customer) yield flavours(key - 1) == value) exists (s => s)
 	}
- 
-	def satisfy (flavours : Array[Boolean], customer : Map[Int, Boolean]) : Boolean = {
-		val k = (for ((key, value) <- customer if value) yield key).toList
-		if (k.size == 0) {
-		  false
-		} else {
-		  flavours(k.head - 1) = true
-		  true
+
+	def unSatisfiedCustomers(flavours : Array[Boolean], customers : List[Map[Int, Boolean]]) : List[Map[Int, Boolean]] = {
+		customers filter (!isSatisfied(flavours, _)) 
+	}
+
+	def solve(flavours : Array[Boolean], customers : List[Map[Int, Boolean]], unsatisfied : List[Map[Int, Boolean]]) : Option[Array[Boolean]] = {
+		unsatisfied.size match {
+		  case 0 => Some(flavours)
+		  case _ => {
+			  satisfyCustomer(flavours, unsatisfied.head).flatMap { solve(_, customers, unSatisfiedCustomers(flavours, customers)) }
+		  }
 		}
 	}
-}
+
+	def satisfyCustomer(flavours : Array[Boolean], customer : Map[Int, Boolean]) : Option[Array[Boolean]] = {
+		val k = (for ((key, value) <- customer if value) yield key).toList
+		if (k.size != 0) {
+			flavours(k.head - 1) = true
+			Some(flavours)
+		} else {
+			None
+		}
+		
+	}
+ }
